22f909a260f5658de93e1c1ee827da9d53782a9f
Sepi <deet1022@gmail.com>
2016-12-09 23:22:23 +0900

    Add project

---
+++ SchoolFinder/SchoolFinder/SchoolInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolFinder
+{
+    /// <summary>
+    /// 학교 정보에 대한 속성들을 제공합니다.
+    /// </summary>
+    public class SchoolInfo
+    {
+        /// <summary>
+        /// 학교의 이름을 제공합니다.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 학교의 주소를 제공합니다.
+        /// </summary>
+        public string Adress { get; set; }
+
+        /// <summary>
+        /// 학교의 고유코드를 제공합니다.
+        /// </summary>
+        public string Code { get; set; }
+    }
+}
